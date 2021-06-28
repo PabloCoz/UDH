389140a930e69bf742b8ea229e750220d5e0c270
Pablo Coz <pablocr_234@hotmail.com>
2021-06-28 15:46:19 -0500

    Pequeño cambio en ImageFaker

--- database/factories/ImageFactory.php
+++ database/factories/ImageFactory.php
@@ -22,7 +22,7 @@ class ImageFactory extends Factory
     public function definition()
     {
         return [
-            'url' => 'courses/'. $this->faker->image(public_path('courses'), 640,480, null, false),
+            'url' => $this->faker->image(public_path('courses'), 640,480, null, false),
         ];
     }
 }
